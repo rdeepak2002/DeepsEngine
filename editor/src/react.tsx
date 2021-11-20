af33ee0f094ca095dc42e32c3e314a3731bc0392
rdeepak2002 <rdeepak2002@gmail.com>
2021-11-20 03:58:23 -0500

    added colored output

--- editor/src/react.tsx
+++ editor/src/react.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import {useState} from "react";
+import {useEffect, useState} from "react";
 const electron = require('electron');
 const { ipcRenderer, remote } = electron;
 
@@ -8,17 +8,19 @@ const Index = () => {
     // message hook
     const [message, setMessage] = useState('no message');
 
-    // send ping message to server
-    ipcRenderer.send('asynchronous-message', 'ping');
+    useEffect(() => {
+        // send ping message to server
+        ipcRenderer.send('asynchronous-message', 'ping');
 
-    // get message from server
-    ipcRenderer.on('asynchronous-reply', (event, arg) => {
-        switch (arg) {
-            case 'pong':
-                setMessage('got pong');
-                break;
-        }
-    })
+        // get message from server
+        ipcRenderer.on('asynchronous-reply', (event, arg) => {
+            switch (arg) {
+                case 'pong':
+                    setMessage('got pong');
+                    break;
+            }
+        });
+    }, []);
 
     // render message
     return <div>{message}</div>;
