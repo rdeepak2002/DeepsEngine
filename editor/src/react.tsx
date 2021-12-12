6a01c09997bd121bb3d2250eda9888677dc8638b
Deepak Ramalingam <rdeepak2002@gmail.com>
2021-12-11 19:09:12 -0500

    added material ui to editor

--- editor/src/react.tsx
+++ editor/src/react.tsx
@@ -1,14 +1,18 @@
-import * as React from 'react';
-import {useEffect, useState} from 'react';
 import * as ReactDOM from 'react-dom';
+import * as React from 'react';
+
+import * as MUI from "@mui/material";
+import {createTheme, PaletteMode} from "@mui/material";
+import {amber, deepOrange, grey} from "@mui/material/colors";
+import {useEffect} from "react";
 
 const electron = require('electron');
 const {ipcRenderer, remote} = electron;
 
 const Index = () => {
     // message hook
-    const [message, setMessage] = useState('no message');
-    const [frameData, setFrameData] = useState(undefined);
+    const [message, setMessage] = React.useState('no message');
+    const [frameData, setFrameData] = React.useState(undefined);
 
     const requestForNewFrame = () => {
         const messageObj = {
@@ -29,10 +33,7 @@ const Index = () => {
         setFrameData(encoded);
     }
 
-    useEffect(() => {
-        // TODO: use callback function for this / call every time render is called in electron
-        // renderLoop();
-
+    React.useEffect(() => {
         // send ping message to server
         const messageObj = {
             name: 'ping',
@@ -85,26 +86,127 @@ const Index = () => {
         ipcRenderer.on('asynchronous-message', messageHandler);
     }, []);
 
+    useEffect(() => {
+        // automatically start renderer
+        const messageObj = {
+            name: 'start-renderer',
+            data: {
+                "createdAt": Date.now()
+            }
+        };
+
+        ipcRenderer.send('asynchronous-message', messageObj);
+    }, []);
+
+    const [mode, setMode] = React.useState<PaletteMode>('dark');
+    const colorMode = React.useMemo(
+        () => ({
+            // The dark mode switch would invoke this method
+            toggleColorMode: () => {
+                setMode((prevMode: PaletteMode) =>
+                    prevMode === 'light' ? 'dark' : 'light',
+                );
+            },
+        }),
+        [],
+    );
+
+    const getDesignTokens = (mode: PaletteMode) => ({
+        palette: {
+            mode,
+            ...(mode === 'light'
+                ? {
+                    // palette values for light mode
+                    // primary: amber,
+                    // divider: amber[200],
+                    // text: {
+                    //     primary: grey[900],
+                    //     secondary: grey[800],
+                    // },
+                }
+                : {
+                    // palette values for dark mode
+                    // primary: deepOrange,
+                    // divider: deepOrange[700],
+                    // background: {
+                    //     default: deepOrange[900],
+                    //     paper: deepOrange[900],
+                    // },
+                    // text: {
+                    //     primary: '#fff',
+                    //     secondary: grey[500],
+                    // },
+                }),
+        },
+    });
+
+    const ColorModeContext = React.createContext({toggleColorMode: () => {}});
+
+    const theme = React.useMemo(() => createTheme(getDesignTokens(mode)), [mode]);
+
     // render message
     return (
-        <>
-            <div>{message}</div>
-            <div>test</div>
-            <button onClick={() => {
-                const messageObj = {
-                    name: 'start-renderer',
-                    data: {
-                        "createdAt": Date.now()
-                    }
-                };
+        <ColorModeContext.Provider value={colorMode}>
+            <MUI.ThemeProvider theme={theme}>
+                <MUI.CssBaseline>
+                    <MUI.Grid container direction="row" alignItems="stretch">
+                        <MUI.Grid item>
+                            <MUI.Typography color="primary" variant="body1">
+                                TODO: scene objects panel
+                            </MUI.Typography>
+                        </MUI.Grid>
 
-                ipcRenderer.send('asynchronous-message', messageObj);
-            }}>Start Renderer
-            </button>
-            {frameData &&
-                <img style={{width: 300, height: 300}} src={frameData}/>
-            }
-        </>
+                        <MUI.Grid item>
+                            <MUI.Grid container direction="column" alignItems="stretch">
+                                <MUI.Grid item>
+                                    <MUI.Typography color="primary" variant="body1">
+                                        TODO: header with play button to run game
+                                    </MUI.Typography>
+                                </MUI.Grid>
+
+                                <MUI.Grid item>
+                                    {frameData ?
+                                        (
+                                            <img style={{width: 300, height: 300}} src={frameData}/>
+                                        )
+                                        :
+                                        (
+                                            <MUI.Typography color="primary" variant="body1">
+                                                loading...
+                                                {/*<MUI.Button variant="contained" onClick={() => {*/}
+                                                {/*    const messageObj = {*/}
+                                                {/*        name: 'start-renderer',*/}
+                                                {/*        data: {*/}
+                                                {/*            "createdAt": Date.now()*/}
+                                                {/*        }*/}
+                                                {/*    };*/}
+
+                                                {/*    ipcRenderer.send('asynchronous-message', messageObj);*/}
+                                                {/*}}>Start Renderer</MUI.Button>*/}
+                                            </MUI.Typography>
+                                        )
+                                    }
+                                </MUI.Grid>
+
+                                <MUI.Grid item>
+                                    <MUI.Typography color="primary" variant="body1">
+                                        TODO: console
+                                    </MUI.Typography>
+                                </MUI.Grid>
+                            </MUI.Grid>
+                        </MUI.Grid>
+
+                        <MUI.Grid item>
+                            <MUI.Typography color="primary" variant="body1">
+                                TODO: inspector
+                            </MUI.Typography>
+                        </MUI.Grid>
+                    </MUI.Grid>
+
+
+                </MUI.CssBaseline>
+            </MUI.ThemeProvider>
+        </ColorModeContext.Provider>
     );
 };
 
