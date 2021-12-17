a55b4d46766ef4f5bf01455002735e441f73f690
Deepak Ramalingam <rdeepak2002@gmail.com>
2021-12-17 14:46:50 -0800

    loads and creates a texture in memory

--- engine/src/Renderer.cpp
+++ engine/src/Renderer.cpp
@@ -163,6 +163,31 @@ int Renderer::init() {
     Shader ourShader(vertexShaderPath.c_str(), fragmentShaderPath.c_str());
     shaderProgramId = ourShader.ID;
 
+    // load and create a texture
+    // -------------------------
+    std::string texturePath = projectPath + "/textures/container.jpg";
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    // set the texture wrapping/filtering options (on the currently bound texture object)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // load and generate the texture
+    int width, height, nrChannels;
+    unsigned char *data = stbi_load(texturePath.c_str(), &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+
     // return 0 on success
     return 0;
 }
