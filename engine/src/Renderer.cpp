0c077fdb6bd90bbf857a7780bd7a8a4364a20f30
Deepak Ramalingam <rdeepak2002@gmail.com>
2021-12-19 15:14:31 -0800

    rotating square

--- engine/src/Renderer.cpp
+++ engine/src/Renderer.cpp
@@ -269,8 +269,8 @@ void Renderer::render() {
     glUseProgram(shaderProgramId);
 
     glm::mat4 trans = glm::mat4(1.0f);
-    trans = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0.0, 0.0, 1.0));
-    trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));
+    trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
+    trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
     unsigned int transformLoc = glGetUniformLocation(shaderProgramId, "transform");
     glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
 
