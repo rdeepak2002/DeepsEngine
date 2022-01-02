12e2e0555b7f3238620002d11b0d2d774f951eb5
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-01-02 13:17:45 -0800

    improved code for transform component interface in inspector view

--- editornative/components/Inspector.tsx
+++ editornative/components/Inspector.tsx
@@ -3,6 +3,7 @@ import {View, Text} from "react-native";
 import FloatInput from "./FloatInput";
 import {genTransform, Transform, TransformStr} from "../interfaces";
 import TransformComponentInput from "./TransformComponentInput";
+import TransformInput from "./TransformInput";
 
 interface InspectorProps {
     selected: number;
@@ -18,50 +19,16 @@ const Inspector = (props: InspectorProps) => {
                 (props.selected >= 0) ? (
                     <View  style={{display: 'flex', flexDirection: 'column'}}>
                         <Text>entity {props.selected}</Text>
-                        <View style={{display: 'flex', flexDirection: 'row'}}>
-                            <View style={{flexGrow: 1}}>
-                                <Text>Position</Text>
-                            </View>
-                            <View style={{width: 10, marginRight: 10}}>
-                                <Text>X</Text>
-                            </View>
-                            <TransformComponentInput
-                                style={{width: 50, marginRight: 10}}
-                                vec3Name={'position'}
-                                vec3Component={'x'}
-                                transformStr={transformStr}
-                                setTransformStr={setTransformStr}
-                                transformVal={transformVal}
-                                setTransformVal={setTransformVal}
-                            />
-                            <View style={{width: 10, marginRight: 10}}>
-                                <Text>Y</Text>
-                            </View>
-                            <TransformComponentInput
-                                style={{width: 50, marginRight: 10}}
-                                vec3Name={'position'}
-                                vec3Component={'y'}
-                                transformStr={transformStr}
-                                setTransformStr={setTransformStr}
-                                transformVal={transformVal}
-                                setTransformVal={setTransformVal}
-                            />
-                            <View style={{width: 10, marginRight: 10}}>
-                                <Text>Z</Text>
-                            </View>
-                            <TransformComponentInput
-                                style={{width: 50, marginRight: 10}}
-                                vec3Name={'position'}
-                                vec3Component={'z'}
-                                transformStr={transformStr}
-                                setTransformStr={setTransformStr}
-                                transformVal={transformVal}
-                                setTransformVal={setTransformVal}
-                            />
-                        </View>
+                        <TransformInput
+                            entitySelected={props.selected}
+                            transformStr={transformStr}
+                            setTransformStr={setTransformStr}
+                            transformVal={transformVal}
+                            setTransformVal={setTransformVal}
+                        />
                     </View>
                 ) : (
-                    <Text>Nothing Selected</Text>
+                    <></>
                 )
             }
         </View>
---
+++ editornative/components/TransformInput.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import {Text, View} from "react-native";
+import TransformComponentInput from "./TransformComponentInput";
+import {Transform, TransformStr} from "../interfaces";
+
+interface TransformInputProps {
+    entitySelected: number;
+    transformStr: TransformStr;
+    setTransformStr: Function;
+    transformVal: Transform;
+    setTransformVal: Function;
+    style?: any;
+}
+
+const TransformInput = (props: TransformInputProps) => {
+    return(
+        <View style={[props.style || {}, {display: 'flex', flexDirection: 'column'}]}>
+            <View>
+                <Text>Transform</Text>
+            </View>
+            {['position', 'rotation', 'scale'].map((transformComponentName, key) => {
+                return (
+                    <View key={`${props.entitySelected}-${transformComponentName}-${key}`} style={{display: 'flex', flexDirection: 'row'}}>
+                        <View style={{flexGrow: 1}}>
+                            <Text>{transformComponentName}</Text>
+                        </View>
+                        {['x', 'y', 'z'].map((vec3ComponentName, key) => {
+                            return (
+                                <View key={`${props.entitySelected}-${transformComponentName}-${vec3ComponentName}-${key}`}  style={{display: 'flex', flexDirection: 'row'}}>
+                                    <View style={{width: 10, marginRight: 10}}>
+                                        <Text>{(vec3ComponentName).toUpperCase()}</Text>
+                                    </View>
+                                    <TransformComponentInput
+                                        style={{width: 50, marginRight: 10}}
+                                        vec3Name={transformComponentName as any}
+                                        vec3Component={vec3ComponentName as any}
+                                        transformStr={props.transformStr}
+                                        setTransformStr={props.setTransformStr}
+                                        transformVal={props.transformVal}
+                                        setTransformVal={props.setTransformVal}
+                                    />
+                                </View>
+                            );
+                        })}
+                    </View>
+                );
+            })}
+        </View>
+    );
+}
+
+export default TransformInput;
