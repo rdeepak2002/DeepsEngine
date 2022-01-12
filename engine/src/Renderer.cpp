f51017fd50aefaad950e45f1002b7487e1e27ff3
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-01-11 23:19:11 -0500

    added camera and pan responder on image view

--- engine/src/Renderer.cpp
+++ engine/src/Renderer.cpp
@@ -352,7 +352,15 @@ void Renderer::render() {
     glm::mat4 view = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
     glm::mat4 projection = glm::mat4(1.0f);
     projection = glm::perspective(glm::radians(45.0f), (float) scrWidth / (float) scrHeight, 0.1f, 100.0f);
-    view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+
+    // old:
+//    view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+    // new:
+    const float radius = 10.0f;
+    float camX = sin(glfwGetTime()) * radius;
+    float camZ = cos(glfwGetTime()) * radius;
+    view = glm::lookAt(glm::vec3(camX, 0.0, camZ), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
+
     // pass transformation matrices to the shader
     ourShader->setMat4("projection",
                        projection); // note: currently we set the projection matrix each frame, but since the projection matrix rarely changes it's often best practice to set it outside the main loop only once.
--- engine/src/Renderer.h
+++ engine/src/Renderer.h
@@ -34,6 +34,14 @@ private:
     Shader *ourShader;
     Coordinator gCoordinator;
 
+    glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+    glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+    glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget);
+    glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+    glm::vec3 cameraRight = glm::normalize(glm::cross(up, cameraDirection));
+    glm::vec3 cameraUp = glm::cross(cameraDirection, cameraRight);
+
+
 public:
     GLFWwindow *window;
     std::string cachedFrame;
