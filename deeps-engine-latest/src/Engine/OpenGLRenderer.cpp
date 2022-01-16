a3a75fe5cccfad13cb4f4b32750430e86f502cd8
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-01-16 00:11:27 -0500

    added keyboard movement

--- deeps-engine-latest/src/Engine/OpenGLRenderer.cpp
+++ deeps-engine-latest/src/Engine/OpenGLRenderer.cpp
@@ -291,6 +291,16 @@ void OpenGLRenderer::processInput(GLFWwindow* window) {
 #if defined(STANDALONE)
   if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
     glfwSetWindowShouldClose(window, true);
+
+  float cameraSpeed = static_cast<float>(2.5 * deltaTime);
+  if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+    cameraPos += cameraSpeed * cameraFront;
+  if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+    cameraPos -= cameraSpeed * cameraFront;
+  if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+    cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+  if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+    cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
 #endif
 }
 
