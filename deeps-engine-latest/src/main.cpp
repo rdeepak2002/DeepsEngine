23e3445e829226ce5235146d2119effbd644af63
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-02-05 03:05:09 -0500

    integrating in old code for rendering opengl

--- deeps-engine-latest/src/main.cpp
+++ deeps-engine-latest/src/main.cpp
@@ -1,3 +1,5 @@
+#if !defined(STANDALONE)
+
 #include <QApplication>
 #include "mainwindow.h"
 
@@ -8,3 +10,33 @@ int main(int argc, char *argv[])
     mainWindow.show();
     return app.exec();
 }
+
+#endif
+
+#if defined(STANDALONE)
+
+#include <iostream>
+
+int main() {
+    std::cout << "hello world" << std::endl;
+}
+
+//#include "OpenGLRenderer.h"
+//
+//int main() {
+//    OpenGLRenderer::createWindow();
+//    OpenGLRenderer::initialize();
+//
+//    // create a single entity for testing purposes
+//    OpenGLRenderer::createEntity();
+//
+//    while (!OpenGLRenderer::shouldCloseWindow()) {
+//        OpenGLRenderer::update();
+//    }
+//
+//    OpenGLRenderer::closeWindow();
+//
+//    return 0;
+//}
+
+#endif
