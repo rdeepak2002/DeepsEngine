69bbf970b432f00d2b073a3430a75b8aa0304f64
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-02-05 00:27:42 -0500

    Fixed entities being listed. TODO: upgrade react node gui version.

--- deeps-engine-latest/src/Editor/Components/Editor/index.tsx
+++ deeps-engine-latest/src/Editor/Components/Editor/index.tsx
@@ -5,17 +5,14 @@ import {widget} from "../OpenGLCube/OpenGLComponent";
 
 const Editor = () => {
     // keep track of entities in scene
-    const [entityIds, setEntityIds] = React.useState<string>("test1");
+    const [entityIds, setEntityIds] = React.useState<any>([]);
 
-    // TODO: create function reference for every time a new entity is created (call this from napi)
-    // TODO: then, call widget.getEntities() in there and set state
-
-    // TODO: maybe have to upgrade node-gui to newer version?
+    // TODO: maybe have to upgrade react node-gui to newer version
 
     /**
      * Handle 'create entity' button being clicked
      */
-    const createEntity = {
+    const handleCreateEntityButton = {
         clicked: () => {
             // create entity
             widget.createEntity();
@@ -26,10 +23,8 @@ const Editor = () => {
      * Callback for every time a new entity is created
      */
     const onEntityCreated = () => {
-        const entitiesJSON: string = JSON.stringify(widget.getEntities());
-        console.log('entities list', entitiesJSON);
-        // setEntityIds(entitiesJSON);
-        // setEntityIds("test")
+        const entityIds: Array<string> = widget.getEntities();
+        setEntityIds(entityIds);
     }
 
     /**
@@ -45,13 +40,14 @@ const Editor = () => {
             <View style={topViewContainer}>
                 <View style={threeColumnContainerStyle}>
                     <View style={sceneItemsContainer}>
-                        <View>
-                            <Text>test 1</Text>
-                        </View>
-                        <View>
-                            <Text>test 2</Text>
-                        </View>
-                        <Button text={"Create Entity"} on={createEntity}/>
+                        {
+                            entityIds.map((entityId: string) => (
+                                <View key={entityId}>
+                                    <Text>{entityId}</Text>
+                                </View>
+                            ))
+                        }
+                        <Button text={"Create Entity"} on={handleCreateEntityButton}/>
                     </View>
                     <View style={rendererContainer}>
                         <OpenGLCube style={rendererScreen}/>
@@ -79,29 +75,36 @@ const threeColumnContainerStyle = `
 `;
 
 const topViewContainer = `
-    flex: 0.9;
+    height: 384;
+    background-color: black;
 `;
 
 const bottomViewContainer = `
-    flex: 0.1;
+    height: 384;
+    background-color: purple;
 `;
 
 const sceneItemsContainer = `
+    width: 341;
+    height: 384;
     background-color: blue;
-    flex: 0.15;
 `;
 
 const rendererContainer = `
-    flex: 0.7;
+    width: 342;
+    height: 384;
+    background-color: red;
 `;
 
 const rendererScreen = `
-    width: '100%';
-    height: '100%';
+    width: 342;
+    height: 384;
 `;
 
 const inspectorContainer = `
-    flex: 0.15;
+    width: 341;
+    height: 384;
+    background-color: green;
 `;
 
 
