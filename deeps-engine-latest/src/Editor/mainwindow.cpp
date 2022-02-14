8653fede000500a983e9de5ad068b3f9c3e7fa70
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-02-14 00:13:58 -0500

    set up basic layout

--- deeps-engine-latest/src/Editor/mainwindow.cpp
+++ deeps-engine-latest/src/Editor/mainwindow.cpp
@@ -5,16 +5,34 @@
 
 MainWindow::MainWindow(QWidget *parent): QMainWindow(parent), ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
-
     // set gl version
     QSurfaceFormat glFormat;
     glFormat.setVersion(3, 3);
     glFormat.setProfile(QSurfaceFormat::CoreProfile);
     QSurfaceFormat::setDefaultFormat(glFormat);
 
+    // ui file
+    ui->setupUi(this);
+
+    // button
+    QPushButton* button = new QPushButton;
+    button->setText("asdf");
+
+    // opengl widget
     OpenGLCubeWidget* openGlCubeWidget = new OpenGLCubeWidget(this);
-    this->layout()->addWidget(openGlCubeWidget);
+
+    // Horizontal layout with 3 buttons
+    QHBoxLayout *hLayout = new QHBoxLayout;
+
+    hLayout->addWidget(openGlCubeWidget);
+
+    // Outer Layer
+    QVBoxLayout *mainLayout = new QVBoxLayout;
+
+    // Add the previous two inner layouts
+    mainLayout->addLayout(hLayout);
+
+    ui->centralwidget->setLayout(mainLayout);
 }
 
 MainWindow::~MainWindow()
