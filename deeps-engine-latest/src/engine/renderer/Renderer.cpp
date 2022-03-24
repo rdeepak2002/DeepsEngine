0ce9ee9e530d3e096a54013e86c7aa2991ac5f3d
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-03-24 00:36:48 -0400

    Fix order of translate, rotate, and scale. Add fixed camera to renderer.

--- deeps-engine-latest/src/engine/renderer/Renderer.cpp
+++ deeps-engine-latest/src/engine/renderer/Renderer.cpp
@@ -243,6 +243,13 @@ void Renderer::update() {
     glm::mat4 view          = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
     glm::mat4 projection    = glm::mat4(1.0f);
     projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+    // TODO: get cameraPos from transform of entity which has camera component
+    // TODO: get cameraPos from transform of entity which has camera component
+    // TODO: get cameraPos from transform of entity which has camera component
+    // TODO: get cameraPos from transform of entity which has camera component
+    // TODO: get cameraPos from transform of entity which has camera component
+    // TODO: get cameraPos from transform of entity which has camera component
+    // TODO: get cameraPos from transform of entity which has camera component (modify code below)
     view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
 
     // pass transformation matrices to the shader
@@ -253,38 +260,36 @@ void Renderer::update() {
     glBindVertexArray(VAO);
 
     // get current scene and all the entities
-//    if (scene) {
-        for(auto entity : scene.GetDrawableEntities()) {
-            // get the entity's transform
-            auto entityTransform = entity.GetComponent<DeepsEngine::Component::Transform>();
-            auto entityPosition = entityTransform.position;
-            auto entityRotation = entityTransform.rotation;
-            auto entityScale = entityTransform.scale;
+    for(auto entity : scene.GetDrawableEntities()) {
+        // get the entity's transform
+        auto entityTransform = entity.GetComponent<DeepsEngine::Component::Transform>();
+        auto entityPosition = entityTransform.position;
+        auto entityRotation = entityTransform.rotation;
+        auto entityScale = entityTransform.scale;
 
-            // calculate the model matrix for each object and pass it to shader before drawing
-            glm::mat4 model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+        // calculate the model matrix for each object and pass it to shader before drawing
+        glm::mat4 model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
 
-            // Scale the model
-            model = glm::scale(model, glm::vec3(entityScale.x, entityScale.y, entityScale.z));
+        // translate the model
+        model = glm::translate(model, glm::vec3(entityPosition.x, entityPosition.y, entityPosition.z));
 
-            // translate the model
-            model = glm::translate(model, glm::vec3(entityPosition.x, entityPosition.y, entityPosition.z));
+        // rotate the model
+        // rotate x
+        model = glm::rotate(model, entityRotation.x, glm::vec3(1.0f, 0.0f, 0.0f));
 
-            // rotate the model
-            // rotate x
-            model = glm::rotate(model, entityRotation.x, glm::vec3(1.0f, 0.0f, 0.0f));
+        // rotate y
+        model = glm::rotate(model, entityRotation.y, glm::vec3(0.0f, 1.0f, 0.0f));
 
-            // rotate y
-            model = glm::rotate(model, entityRotation.y, glm::vec3(0.0f, 1.0f, 0.0f));
+        // rotate z
+        model = glm::rotate(model, entityRotation.z, glm::vec3(0.0f, 0.0f, 1.0f));
 
-            // rotate z
-            model = glm::rotate(model, entityRotation.z, glm::vec3(0.0f, 0.0f, 1.0f));
+        // Scale the model
+        model = glm::scale(model, glm::vec3(entityScale.x, entityScale.y, entityScale.z));
 
-            ourShader->setMat4("model", model);
+        ourShader->setMat4("model", model);
 
-            glDrawArrays(GL_TRIANGLES, 0, 36);
-        }
-//    }
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+    }
 
 #if defined(STANDALONE)
     // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
