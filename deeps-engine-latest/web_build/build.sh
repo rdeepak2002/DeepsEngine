9c3bbc0ec82776d5efb05347211e32638e30f9df
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-03-26 23:14:22 -0400

    Update script for web build

--- deeps-engine-latest/web_build/build.sh
+++ deeps-engine-latest/web_build/build.sh
@@ -1,9 +1,20 @@
 export STANDALONE="True"
 
-cd build
-emcmake cmake ..
+# remove current source code
+rm -rf src
+
+# create new folder for source code
+mkdir -p src
+cd src
+
+# build source code
+emcmake cmake ../..
 emcc -s USE_SDL=2 $(mktemp)
 emcc -s USE_ZLIB=1 $(mktemp)
 make
 
+# go back to root
+cd ..
+
+# serve content
 http-server -o "/app"
