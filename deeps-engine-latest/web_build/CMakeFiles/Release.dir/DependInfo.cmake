82ce05454a77905d57ba3e9df739235b7f43d64e
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-03-26 20:15:59 -0400

    Added changes

---
+++ deeps-engine-latest/web_build/CMakeFiles/Release.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/deepakramalingam/Documents/Projects/deeps-engine/deeps-engine-latest/src/engine/include/glad.c" "CMakeFiles/Release.dir/src/engine/include/glad.c.o" "gcc" "CMakeFiles/Release.dir/src/engine/include/glad.c.o.d"
+  "/Users/deepakramalingam/Documents/Projects/deeps-engine/deeps-engine-latest/src/engine/Logger.cpp" "CMakeFiles/Release.dir/src/engine/Logger.cpp.o" "gcc" "CMakeFiles/Release.dir/src/engine/Logger.cpp.o.d"
+  "/Users/deepakramalingam/Documents/Projects/deeps-engine/deeps-engine-latest/src/engine/renderer/Renderer.cpp" "CMakeFiles/Release.dir/src/engine/renderer/Renderer.cpp.o" "gcc" "CMakeFiles/Release.dir/src/engine/renderer/Renderer.cpp.o.d"
+  "/Users/deepakramalingam/Documents/Projects/deeps-engine/deeps-engine-latest/src/engine/renderer/Shader.cpp" "CMakeFiles/Release.dir/src/engine/renderer/Shader.cpp.o" "gcc" "CMakeFiles/Release.dir/src/engine/renderer/Shader.cpp.o.d"
+  "/Users/deepakramalingam/Documents/Projects/deeps-engine/deeps-engine-latest/src/engine/scene/Entity.cpp" "CMakeFiles/Release.dir/src/engine/scene/Entity.cpp.o" "gcc" "CMakeFiles/Release.dir/src/engine/scene/Entity.cpp.o.d"
+  "/Users/deepakramalingam/Documents/Projects/deeps-engine/deeps-engine-latest/src/engine/scene/Scene.cpp" "CMakeFiles/Release.dir/src/engine/scene/Scene.cpp.o" "gcc" "CMakeFiles/Release.dir/src/engine/scene/Scene.cpp.o.d"
+  "/Users/deepakramalingam/Documents/Projects/deeps-engine/deeps-engine-latest/src/game.cpp" "CMakeFiles/Release.dir/src/game.cpp.o" "gcc" "CMakeFiles/Release.dir/src/game.cpp.o.d"
+  "/Users/deepakramalingam/Documents/Projects/deeps-engine/deeps-engine-latest/src/main.cpp" "CMakeFiles/Release.dir/src/main.cpp.o" "gcc" "CMakeFiles/Release.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ deeps-engine-latest/web_build/CMakeFiles/Release.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Release.dir/src/engine/Logger.cpp.o"
+  "CMakeFiles/Release.dir/src/engine/Logger.cpp.o.d"
+  "CMakeFiles/Release.dir/src/engine/include/glad.c.o"
+  "CMakeFiles/Release.dir/src/engine/include/glad.c.o.d"
+  "CMakeFiles/Release.dir/src/engine/renderer/Renderer.cpp.o"
+  "CMakeFiles/Release.dir/src/engine/renderer/Renderer.cpp.o.d"
+  "CMakeFiles/Release.dir/src/engine/renderer/Shader.cpp.o"
+  "CMakeFiles/Release.dir/src/engine/renderer/Shader.cpp.o.d"
+  "CMakeFiles/Release.dir/src/engine/scene/Entity.cpp.o"
+  "CMakeFiles/Release.dir/src/engine/scene/Entity.cpp.o.d"
+  "CMakeFiles/Release.dir/src/engine/scene/Scene.cpp.o"
+  "CMakeFiles/Release.dir/src/engine/scene/Scene.cpp.o.d"
+  "CMakeFiles/Release.dir/src/game.cpp.o"
+  "CMakeFiles/Release.dir/src/game.cpp.o.d"
+  "CMakeFiles/Release.dir/src/main.cpp.o"
+  "CMakeFiles/Release.dir/src/main.cpp.o.d"
+  "Release.js"
+  "Release.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/Release.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
