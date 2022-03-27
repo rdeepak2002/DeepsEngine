c3e0522fb152b7cb4aec3d6eab329fff3bea9bd4
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-03-27 02:24:46 -0400

    getting closer, fixing shader compilation errors right now

--- deeps-engine-latest/src/main.cpp
+++ deeps-engine-latest/src/main.cpp
@@ -11,52 +11,89 @@
 #define EGL_EGLEXT_PROTOTYPES
 #include <GLFW/glfw3.h>
 #else
-//#include <glad.h>
+#include <glad.h>
 #include <glfw3.h>
 #endif
 
 // For example purpose we use global variables
 #define SPEED 0.005f
 GLFWwindow* window = nullptr;
-float red = 0.0f;
-bool increase = true;
+//float red = 0.0f;
+//bool increase = true;
 
-/**
- * @brief This function just increases and decreases the red value.
- *
- * @return The red value.
- */
-float getRed() {
-    if (increase) {
-        red += SPEED;
-        if (red > 1.0f) {
-            red = 1.0f;
-            increase = false;
-        }
-    } else {
-        red -= SPEED;
-        if (red < 0.0f) {
-            red = 0.0f;
-            increase = true;
-        }
-    }
+unsigned int VBO, VAO;
+unsigned int shaderProgram;
 
-    return red;
-}
+const char *vertexShaderSource = "#version 330 core\n"
+                                 "layout (location = 0) in vec3 aPos;\n"
+                                 "void main()\n"
+                                 "{\n"
+                                 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                 "}\0";
+const char *fragmentShaderSource = "#version 330 core\n"
+                                   "out vec4 FragColor;\n"
+                                   "void main()\n"
+                                   "{\n"
+                                   "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                                   "}\n\0";
+
+///**
+// * @brief This function just increases and decreases the red value.
+// *
+// * @return The red value.
+// */
+//float getRed() {
+//    if (increase) {
+//        red += SPEED;
+//        if (red > 1.0f) {
+//            red = 1.0f;
+//            increase = false;
+//        }
+//    } else {
+//        red -= SPEED;
+//        if (red < 0.0f) {
+//            red = 0.0f;
+//            increase = true;
+//        }
+//    }
+//
+//    return red;
+//}
 
 /**
  * @brief This function is called every frame.
  */
 void mainLoop() {
-    // Clear the screen with a color
-    glClearColor(getRed(), 0.0f, 0.0f, 1.0f);
+//    // Clear the screen with a color
+//    glClearColor(getRed(), 0.0f, 0.0f, 1.0f);
+//    glClear(GL_COLOR_BUFFER_BIT);
+//
+//    // Swap the buffers of the window
+//    glfwSwapBuffers(window);
+//
+//    // Poll for the events
+//    glfwPollEvents();
+///
+//    // input
+//    // -----
+//    processInput(window);
+
+    // render
+    // ------
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    // Swap the buffers of the window
-    glfwSwapBuffers(window);
+    // draw our first triangle
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    // glBindVertexArray(0); // no need to unbind it every time
 
-    // Poll for the events
+    // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+    // -------------------------------------------------------------------------------
+    glfwSwapBuffers(window);
     glfwPollEvents();
+///
 }
 
 /**
@@ -81,30 +118,75 @@ int main() {
     // Make this window the current context
     glfwMakeContextCurrent(window);
 
-#ifdef EMSCRIPTEN
-//gladLoadGLLoader((GLADloadproc) emscripten_GetProcAddress);
-#else
-// glad: load all OpenGL function pointers
-// ---------------------------------------
-//if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-//{
-//    std::cout << "Failed to initialize GLAD" << std::endl;
-//    return -1;
-//}
-//gladLoadGL();
-#endif
+    ///
 
-//    unsigned int VBO, VAO;
-//    glGenVertexArrays(1, &VAO);
-//    glGenBuffers(1, &VBO);
-//    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-//    glBindVertexArray(VAO);
+    // build and compile our shader program
+    // ------------------------------------
+    // vertex shader
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    // check for shader compile errors
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    // fragment shader
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    // check for shader compile errors
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    // link shaders
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    // check for linking errors
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
 
-    unsigned int VBO, VAO;
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // ------------------------------------------------------------------
+    float vertices[] = {
+            -0.5f, -0.5f, 0.0f, // left
+            0.5f, -0.5f, 0.0f, // right
+            0.0f,  0.5f, 0.0f  // top
+    };
+
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
     // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
     glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+    glBindVertexArray(0);
+
+    ///
 
     std::cout << "Going into loop" << std::endl;
 #ifdef EMSCRIPTEN
