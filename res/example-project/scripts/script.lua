9b94ffcf410468f23a56cf77f5f135935cc27cb8
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-04-14 01:03:44 -0400

    Add blank onUpdate function

--- res/example-project/scripts/script.lua
+++ res/example-project/scripts/script.lua
@@ -1,3 +1,9 @@
 function onCreate(entity)
+    print("On create function")
     entity:GetTransform().position.x = 2.0
+end
+
+function onUpdate(entity, dt)
+    print("On update function")
+    print(dt)
 end
