35f9b205100cf0d5ef03a34634f6e5e87ca69f26
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-04-18 00:59:50 -0400

    Found reason for stutter for rotation via script

--- res/example-project/scripts/script.lua
+++ res/example-project/scripts/script.lua
@@ -1,7 +1,14 @@
+local totalTime = 0.0
+
 function onCreate(self)
-    self:GetTransform().rotation.y = 0.0
+    Logger.Debug("On Create")
+    -- TODO: fix issue where on create is randomly called which causes rotation stutter
+    -- self:GetTransform().rotation.y = 0.0
 end
 
 function onUpdate(self, dt)
+    totalTime = totalTime + dt
+    Logger.Debug("Total time in script:")
+    Logger.Debug(tostring(totalTime))
     self:GetTransform().rotation.y = self:GetTransform().rotation.y + 1.0 * dt
 end
