c981290c2339e339d0e1e2f5ec10645d5daebb5a
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-05-01 21:28:52 -0400

    Create blank input class

---
+++ src/engine/Input.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Deepak Ramalingam on 5/1/22.
+//
+
+#include "Input.h"
---
+++ src/engine/Input.h
@@ -0,0 +1,14 @@
+//
+// Created by Deepak Ramalingam on 5/1/22.
+//
+
+#ifndef EDITOR_INPUT_H
+#define EDITOR_INPUT_H
+
+
+class Input {
+
+};
+
+
+#endif //EDITOR_INPUT_H
