c449416519b6cf45384e575228bb5a95e528093c
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-05-07 03:51:52 -0400

    Modified script so resource directory is provided

--- src/build/web/build.sh
+++ src/build/web/build.sh
@@ -3,6 +3,16 @@
 # allow modification of this folder
 sudo chmod -R 777 ./
 
+echo ${DEEPS_ENGINE_RESOURCE_DIRECTORY}
+
+if [[ -v DEEPS_ENGINE_RESOURCE_DIRECTORY ]];
+then
+    echo "Resource directory set to ${DEEPS_ENGINE_RESOURCE_DIRECTORY}"
+else
+    echo "Error: DEEPS_ENGINE_RESOURCE_DIRECTORY env variable not provided!"
+    exit 1
+fi
+
 # download emscripten
 ./download-emscripten.sh
 source emsdk/emsdk_env.sh
@@ -15,12 +25,12 @@ export VCPKG_ROOT="$(pwd)/vcpkg"
 echo "Creating web build..."
 
 # remove current source code
-rm -rf src
+rm -rf build
 
 # create new folder for source code
 mkdir -p build/assets
 cp app.html build/app.html
-cp -R /Users/deepakramalingam/Documents/Projects/DeepsEngine/res build/assets/res
+cp -R ${DEEPS_ENGINE_RESOURCE_DIRECTORY} build/assets/res
 
 # build source code
 cmake -S ./ -B build "-DVCPKG_CHAINLOAD_TOOLCHAIN_FILE=$(pwd)/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake" "-DCMAKE_TOOLCHAIN_FILE=$(pwd)/vcpkg/scripts/buildsystems/vcpkg.cmake" "-DVCPKG_TARGET_TRIPLET=wasm32-emscripten"
