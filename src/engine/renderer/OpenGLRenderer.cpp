3d5501a24a8210fe69eac20a7a76c669ab567755
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-08-15 00:48:37 -0400

    Embed frame buffer in ImGui window

--- src/engine/renderer/OpenGLRenderer.cpp
+++ src/engine/renderer/OpenGLRenderer.cpp
@@ -68,6 +68,17 @@ float skyboxVertices[] = {
         1.0f, -1.0f,  1.0f
 };
 
+float quadVertices[] = { // vertex attributes for a quad that fills the entire screen in Normalized Device Coordinates.
+        // positions   // texCoords
+        -1.0f,  1.0f,  0.0f, 1.0f,
+        -1.0f, -1.0f,  0.0f, 0.0f,
+        1.0f, -1.0f,  1.0f, 0.0f,
+
+        -1.0f,  1.0f,  0.0f, 1.0f,
+        1.0f, -1.0f,  1.0f, 0.0f,
+        1.0f,  1.0f,  1.0f, 1.0f
+};
+
 void OpenGLRenderer::initialize() {
     Logger::Debug("Initializing renderer");
 
@@ -83,6 +94,17 @@ void OpenGLRenderer::initialize() {
 //    // have qt initialize opengl functions
 //    initializeOpenGLFunctions();
 //#endif
+
+    // screen quad VAO
+    glGenVertexArrays(1, &quadVAO);
+    glGenBuffers(1, &quadVBO);
+    glBindVertexArray(quadVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
 
     // configure global opengl state
     // -----------------------------
@@ -111,6 +133,13 @@ void OpenGLRenderer::initialize() {
             Application::getInstance().getProjectPath().append("src").append("shaders").append("physics.vert").c_str(),
             Application::getInstance().getProjectPath().append("src").append("shaders").append("physics.frag").c_str());
 
+    screenShader = new Shader(
+            Application::getInstance().getProjectPath().append("src").append("shaders").append("framebuffers_screen.vert").c_str(),
+            Application::getInstance().getProjectPath().append("src").append("shaders").append("framebuffers_screen.frag").c_str());
+
+    screenShader->use();
+    screenShader->setInt("screenTexture", 0);
+
     // skybox VAO
     glGenVertexArrays(1, &skyboxVAO);
     glGenBuffers(1, &skyboxVBO);
@@ -137,13 +166,46 @@ void OpenGLRenderer::initialize() {
     // --------------------
     skyboxShader->use();
     skyboxShader->setInt("skybox", 0);
+
+    updateFrameBuffer();
+}
+
+void OpenGLRenderer::updateFrameBuffer() {
+    GLint dims[4] = {0};
+    glGetIntegerv(GL_VIEWPORT, dims);
+    GLint fbWidth = dims[2];
+    GLint fbHeight = dims[3];
+
+    // framebuffer configuration
+    // -------------------------
+    glGenFramebuffers(1, &framebuffer);
+    glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+    // create a color attachment texture
+    glGenTextures(1, &textureColorbuffer);
+    glBindTexture(GL_TEXTURE_2D, textureColorbuffer);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, fbWidth, fbHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureColorbuffer, 0);
+    // create a renderbuffer object for depth and stencil attachment (we won't be sampling these)
+    glGenRenderbuffers(1, &rbo);
+    glBindRenderbuffer(GL_RENDERBUFFER, rbo);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, fbWidth, fbHeight); // use a single renderbuffer object for both a depth AND stencil buffer.
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo); // now actually attach it
+    // now that we actually created the framebuffer and added all attachments we want to check if it is actually complete now
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+        cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!" << endl;
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
 }
 
 void OpenGLRenderer::clear() {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+//    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
 
 void OpenGLRenderer::update() {
+    // bind to framebuffer and draw scene as we normally would to color texture
+    glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+
     // TODO: figure out why we need this here for QT renderer
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_BLEND);
@@ -264,6 +326,17 @@ void OpenGLRenderer::update() {
         // TODO: write text there is no camera
     }
 
+    // now bind back to default framebuffer and draw a quad plane with the attached framebuffer color texture
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    glDisable(GL_DEPTH_TEST); // disable depth test so screen-space quad isn't discarded due to depth test.
+    // clear all relevant buffers
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f); // set clear color to white (not really necessary actually, since we won't be able to see behind the quad anyways)
+    glClear(GL_COLOR_BUFFER_BIT);
+    screenShader->use();
+    glBindVertexArray(quadVAO);
+    glBindTexture(GL_TEXTURE_2D, textureColorbuffer);	// use the color attachment texture as the texture of the quad plane
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+
     // render your GUI
     if(ImGui::Begin("DeepsEngine", NULL, 0))
     {
@@ -271,10 +344,13 @@ void OpenGLRenderer::update() {
         // It also allows customization
         if(ImGui::BeginChild("GameRender"))
         {
-            ImGui::Button("Hello!");
-//            // Because I use the texture from OpenGL, I need to invert the V from the UV.
-//            ImGui::Image(reinterpret_cast<ImTextureID>(GL_TEXTURE_HANDLE), wsize, ImVec2(0, 1), ImVec2(1, 0));
-//            ImGui::EndChild();
+            // Get the size of the child (i.e. the whole draw size of the windows).
+//            ImVec2 wsize = ImGui::GetWindowSize();
+            ImVec2 wsize = ImVec2(float(SCR_WIDTH) / 4, float(SCR_HEIGHT) / 4);
+            // Because I use the texture from OpenGL, I need to invert the V from the UV.
+//            ImGui::Image((ImTextureID)framebuffer, wsize, ImVec2(0, 1), ImVec2(1, 0));
+            ImGui::Image(reinterpret_cast<ImTextureID>(textureColorbuffer), wsize, ImVec2(0, 1), ImVec2(1, 0));
+            ImGui::EndChild();
         }
 
         ImGui::End();
